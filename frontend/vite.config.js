03c2402a41532467f359173ccb6d4216dd6bd7ff
phamhuynhtotram <phamhuynhtotram@gmail.com>
2025-11-26 18:56:29 +0700

    Final Fix: Leaflet Rollup configuration and syntax fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './', 
+  
   build: {
     assetsInlineLimit: 4096, 
+    rollupOptions: {
+      external: [/^leaflet\/dist\/images\//],
+    },
+  },
+  
+  optimizeDeps: {
+    include: ['react-leaflet', 'leaflet'],
+    exclude: ['react-leaflet'], 
   },
 });

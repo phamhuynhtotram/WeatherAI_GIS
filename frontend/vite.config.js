a8dce59841258527d143b9e7b16482ff0c0e92da
phamhuynhtotram <phamhuynhtotram@gmail.com>
2025-11-26 21:28:31 +0700

    Fix: Set vite base to / to resolve 404 Not Found error on deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,20 @@
-// file: vite.config.js 
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+    import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: './',
+    export default defineConfig({
+      plugins: [react()],
+      base: '/', 
 
-  build: {
-    assetsInlineLimit: 4096,
-  },
-  
-  optimizeDeps: {
-    include: ['react-leaflet', 'leaflet'], 
-  },
-});
+      build: {
+        assetsInlineLimit: 4096,
+      },
+      
+      optimizeDeps: {
+        include: ['react-leaflet', 'leaflet'],
+        esbuildOptions: {
+          loader: {
+            '.js': 'jsx', 
+          },
+        },
+      },
+    });
